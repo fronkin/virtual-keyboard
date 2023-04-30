79ce36f2406da1010990f0b64f331d6a7827b9de
fronkin <feko992@gmail.com>
2023-04-30 16:21:34 +0300

    feat: add style css

---
+++ ru.js
@@ -0,0 +1,104 @@
+export const ru = () =>{
+    const container = document.createElement('div');
+    container.classList.add('container');
+    
+    const nightMode = document.createElement('div');
+    nightMode.classList.add('night_mode');
+    
+    const toggleCircle = document.createElement('div');
+    toggleCircle.classList.add('toggle_circle');
+    nightMode.append(toggleCircle);
+    
+    const changeLightColor = document.createElement('div');
+    changeLightColor.classList.add('change_light_color');
+    
+    const colors = document.createElement('div');
+    colors.classList.add('colors');
+    
+    const colorsInput = document.createElement('input');
+    colorsInput.classList.add('colors_input');
+    colorsInput.setAttribute('type', 'color');
+    
+    colors.append(colorsInput);
+    changeLightColor.append(colors);
+    
+    const keyboardWrapp = document.createElement('div');
+    keyboardWrapp.classList.add('keyboard_wrapp');
+    
+    const keyboardLights = document.createElement('div');
+    keyboardLights.classList.add('keyboard_lights');
+    
+    const keyboardKeys = document.createElement('div');
+    keyboardKeys.classList.add('keyboard_keys');
+    
+    const row1 = document.createElement('div');
+    row1.classList.add('row');
+    
+    const keys1 = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'];
+    keys1.forEach(key => {
+      const keyElement = document.createElement('div');
+      keyElement.classList.add('keys');
+      keyElement.textContent = key;
+      row1.append(keyElement);
+    });
+    keyboardKeys.append(row1);
+    
+    const row2 = document.createElement('div');
+    row2.classList.add('row');
+    
+    const keys2 = ['Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\\'];
+    keys2.forEach(key => {
+      const keyElement = document.createElement('div');keyElement.classList.add('keys');
+      keyElement.textContent = key;
+      row2.append(keyElement);
+    });
+    keyboardKeys.append(row2);
+    
+    const row3 = document.createElement('div');
+    row3.classList.add('row');
+    
+    const keys3 = ['Caps Lock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', "Э", 'Enter'];
+    keys3.forEach(key => {
+      const keyElement = document.createElement('div');
+      keyElement.classList.add('keys');
+      keyElement.textContent = key;
+      row3.append(keyElement);
+    });
+    keyboardKeys.append(row3);
+    
+    const row4 = document.createElement('div');
+    row4.classList.add('row');
+    
+    const keys4 = ['Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '/', '?', 'Shift'];
+    keys4.forEach(key => {
+      const keyElement = document.createElement('div');
+      keyElement.classList.add('keys');
+      keyElement.textContent = key;
+      row4.append(keyElement);
+    });
+    keyboardKeys.append(row4);
+    
+    const row5 = document.createElement('div');
+    row5.classList.add('row');
+    
+    const keys5 = ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Fn', 'Ctrl']; keys5.forEach(key => {
+      const keyElement = document.createElement('div');
+      keyElement.classList.add('keys');
+      keyElement.textContent = key;
+      row5.append(keyElement);
+    });
+    keyboardKeys.append(row5);
+    
+    const textInput = document.createElement('input');
+    textInput.classList.add('text');
+    textInput.setAttribute('type', 'text');
+    
+    // Добавляем элементы на страницу
+    keyboardWrapp.append(keyboardLights);
+    keyboardWrapp.append(keyboardKeys);
+    container.append(nightMode);
+    container.append(changeLightColor);
+    container.append(keyboardWrapp);
+    container.append(textInput);
+    document.body.append(container);
+}
