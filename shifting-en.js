d7026346b88fbf8a8b8defb0e638ec370486ba22
fronkin <feko992@gmail.com>
2023-05-02 22:25:31 +0300

    fix:ESLint 2.0

--- shifting-en.js
+++ shifting-en.js
@@ -1,124 +1,124 @@
+/* eslint linebreak-style: ["error", "windows"] */
 export const shiftingEn = (text) => {
   switch (text) {
-    case "←":
-      return "←";
-    case "↓":
-      return "↓";
-    case "↑":
-      return "↑";
-    case "→":
-      return "→";
-    case "`":
-      return "~";
-    case "1":
-      return "!";
-    case "2":
-      return "@";
-    case "3":
-      return "#";
-    case "4":
-      return "$";
-    case "5":
-      return "%";
-    case "6":
-      return "^";
-    case "7":
-      return "&";
-    case "8":
-      return "*";
-    case "9":
-      return "(";
-    case "0":
-      return ")";
-    case "-":
-      return "_";
-    case "=":
-      return "+";
-    case "[":
-      return "{";
-    case "]":
-      return "}";
-    case "\\":
-      return "|";
-    case ";":
-      return ":";
+    case '←':
+      return '←';
+    case '↓':
+      return '↓';
+    case '↑':
+      return '↑';
+    case '→':
+      return '→';
+    case '`':
+      return '~';
+    case '1':
+      return '!';
+    case '2':
+      return '@';
+    case '3':
+      return '#';
+    case '4':
+      return '$';
+    case '5':
+      return '%';
+    case '6':
+      return '^';
+    case '7':
+      return '&';
+    case '8':
+      return '*';
+    case '9':
+      return '(';
+    case '0':
+      return ')';
+    case '-':
+      return '_';
+    case '=':
+      return '+';
+    case '[':
+      return '{';
+    case ']':
+      return '}';
+    case '\\':
+      return '|';
+    case ';':
+      return ':';
     case "'":
       return '"';
-    case ",":
-      return "<";
-    case ".":
-      return ">";
-    case "/":
-      return "?";
-    case " ":
-      return " ";
-    case "Caps Lock":
-      return "Caps Lock";
+    case ',':
+      return '<';
+    case '.':
+      return '>';
+    case '/':
+      return '?';
+    case ' ':
+      return ' ';
+    case 'Caps Lock':
+      return 'Caps Lock';
 
     default:
       return text;
   }
-}
+};
 
 export const downingEn = (text) => {
-    switch (text) {
-      case "←":
-        return "←";
-      case "↓":
-        return "↓";
-      case "↑":
-        return "↑";
-      case "→":
-        return "→";
-      case "~":
-        return "`";
-      case "!":
-        return "1";
-      case "@":
-        return "2";
-      case "#":
-        return "3";
-      case "$":
-        return "4";
-      case "%":
-        return "5";
-      case "^":
-        return "6";
-      case "&":
-        return "7";
-      case "*":
-        return "8";
-      case "(":
-        return "9";
-      case ")":
-        return "0";
-      case "_":
-        return "-";
-      case "+":
-        return "=";
-      case "{":
-        return "[";
-      case "}":
-        return "]";
-      case "|":
-        return "\\";
-      case ":":
-        return ";";
-      case '"':
-        return "'";
-      case "<":
-        return ",";
-      case ">":
-        return ".";
-      case "?":
-        return "/";
-      case " ":
-        return " ";
-      case "Caps Lock":
-        return "Caps Lock";
-  
-        default:
-            return text;
-    }
+  switch (text) {
+    case '←':
+      return '←';
+    case '↓':
+      return '↓';
+    case '↑':
+      return '↑';
+    case '→':
+      return '→';
+    case '~':
+      return '`';
+    case '!':
+      return '1';
+    case '@':
+      return '2';
+    case '#':
+      return '3';
+    case '$':
+      return '4';
+    case '%':
+      return '5';
+    case '^':
+      return '6';
+    case '&':
+      return '7';
+    case '*':
+      return '8';
+    case '(':
+      return '9';
+    case ')':
+      return '0';
+    case '_':
+      return '-';
+    case '+':
+      return '=';
+    case '{':
+      return '[';
+    case '}':
+      return ']';
+    case '|':
+      return '\\';
+    case ':':
+      return ';';
+    case '"':
+      return "'";
+    case '<':
+      return ',';
+    case '>':
+      return '.';
+    case '?':
+      return '/';
+    case ' ':
+      return ' ';
+    case 'Caps Lock':
+      return 'Caps Lock';
+
+    default:
+      return text;
   }
-  
+};
